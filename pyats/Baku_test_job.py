1e2539a5e855b56ad42d3ea86bf9927bcd7cf9bf
Shaun Chivers (Europe) <shaun.chivers@global.ntt>
2021-04-22 21:19:15 +0100

    Add new file

---
+++ pyats/Baku_test_job.py
@@ -0,0 +1,43 @@
+"""
+Baku_test_job.py
+"""
+# see https://pubhub.devnetcloud.com/media/pyats/docs/easypy/jobfile.html
+# for how job files work
+
+__author__ = "Shaun Chivers"
+__copyright__ = "NTT Global"
+__contact__ = ["shaun.chivers@global.ntt"]
+__credits__ = ["Hank Preston, Cisco Systems"]
+__version__ = 1.0
+
+import os
+from pyats.easypy import run
+
+# compute the script path from this location
+SCRIPT_PATH = os.path.dirname(__file__)
+
+
+def main(runtime):
+    """job file entrypoint"""
+
+    # run script
+    run(
+        testscript=os.path.join(SCRIPT_PATH, "testbed_connectivity.py"),
+        runtime=runtime,
+        taskid="Validate Connectivity",
+    )
+#    run(
+#        testscript=os.path.join(SCRIPT_PATH, "hostname_checker.py"),
+#        runtime=runtime,
+#        taskid="Check Configured Hostname",
+#     )
+    run(
+        testscript=os.path.join(SCRIPT_PATH, "interface_errors.py"),
+        runtime=runtime,
+        taskid="Interface Errors",
+    )
+    run(
+       testscript=os.path.join(SCRIPT_PATH, "version_checker.py"),
+       runtime=runtime,
+       taskid="Version Checker",
+    )
