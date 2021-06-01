f43b657e5c8b33764516cb026ada93af6122465d
Shaun Chivers (Europe) <shaun.chivers@global.ntt>
2021-06-01 13:57:12 +0100

    corrected path

--- pyats/build_test_job.py
+++ pyats/build_test_job.py
@@ -25,62 +25,62 @@ def main(runtime):
 
     # run script
     run(
-       testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/version_checker/version_checker.py"),
+       testscript=os.path.join(SCRIPT_PATH, "./version_checker/version_checker.py"),
        runtime=runtime,
        taskid="Version Checker",
      )
     run(
-       testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/check_cdp/cdp_enabled.py"),
+       testscript=os.path.join(SCRIPT_PATH, "./check_cdp/cdp_enabled.py"),
        runtime=runtime,
        taskid="Check CDP is enabled",
      )
     run(
-       testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/check_lldp/check_lldp.py"),
+       testscript=os.path.join(SCRIPT_PATH, "./check_lldp/check_lldp.py"),
        runtime=runtime,
        taskid="Check LLDP is enabled",
      )
     run(
-       testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/check_snmp/check_snmp_settings.py"),
+       testscript=os.path.join(SCRIPT_PATH, "./check_snmp/check_snmp_settings.py"),
        runtime=runtime,
        taskid="Check SNMP settings",
      )
     run(
-      testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/check_dns/check_dns_server_settings.py"),
+      testscript=os.path.join(SCRIPT_PATH, "./check_dns/check_dns_server_settings.py"),
       runtime=runtime,
       taskid="Check DNS Server Settings",
      )
     run(
-      testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/errdisabled/errdisabled.py"),
+      testscript=os.path.join(SCRIPT_PATH, "./errdisabled/errdisabled.py"),
       runtime=runtime,
       taskid="Check Errdisabled Configuration",
      )
     run(
-      testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/check_dns_server_settings/check_dns_server_settings.py"),
+      testscript=os.path.join(SCRIPT_PATH, "./check_dns_server_settings/check_dns_server_settings.py"),
       runtime=runtime,
       taskid="Check DNS Server Settings",
      )
     run(
-      testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/check_ntp/check_ntp_associations.py"),
+      testscript=os.path.join(SCRIPT_PATH, "./check_ntp/check_ntp_associations.py"),
       runtime=runtime,
       taskid="Check NTP Settings",
      )
     run(
-      testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/local_users/local_user_check.py"),                   
+      testscript=os.path.join(SCRIPT_PATH, "./local_users/local_user_check.py"),                   
       runtime=runtime,
       taskid="Check Local Users",       
      )
     run(
-      testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/remote_management_access/remote_manage.py"),                         
+      testscript=os.path.join(SCRIPT_PATH, "./remote_management_access/remote_manage.py"),                         
       runtime=runtime,
       taskid="Check Device Management (Hostname, Creds, Domain name, crypto, SSH Version, VTY SSH Input, AAA, Enable secret)",        
      )
     run(
-      testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/syslog/check_syslog.py"),                            
+      testscript=os.path.join(SCRIPT_PATH, "./syslog/check_syslog.py"),                            
       runtime=runtime,
       taskid="Check Syslog)",        
      )
     run(
-      testscript=os.path.join(SCRIPT_PATH, "/home/ntt/source-of-truth/pyats/running_vs_startup/run_vs_start.py"),
+      testscript=os.path.join(SCRIPT_PATH, "./running_vs_startup/run_vs_start.py"),
       runtime=runtime,
      taskid="Compare runnning-config to startup-config",
      )
