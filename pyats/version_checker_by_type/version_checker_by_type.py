3d37b965c50c0a6d1723def2cada421f9b97a193
Shaun Chivers (Europe) <shaun.chivers@global.ntt>
2021-06-19 13:39:39 +0100

    Added support for WS-C2960L-48PQ-LL running 15.2(7)E3

--- pyats/version_checker_by_type/version_checker_by_type.py
+++ pyats/version_checker_by_type/version_checker_by_type.py
@@ -32,6 +32,7 @@ software_version_dictionary = {
     "Catalyst 9500-40X": "16.12.4",
     "Catalyst WS-C2960L-24PS-LL": "15.2(7)E3",
     "Catalyst WS-C2960L-48PS-LL": "15.2(7)E3",
+    "Catalyst WS-C2960L-48PQ-LL": "15.2(7)E3",
     "WS-C3650-12X48UR": "16.09.06",
     "ASR1001-HX": "16.12.5",
 }
